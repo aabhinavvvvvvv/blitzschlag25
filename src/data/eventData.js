39ee0851b20776e00e61620d39f13c62bd1c38a3
Sachin-og <2022uec1128@mnit.ac.in>
2025-01-11 02:08:57 +0530

    main100

--- src/data/eventData.js
+++ src/data/eventData.js
@@ -227,53 +227,53 @@ const eventData = {
         club: 'none',
         type: 'attraction',
     },
-    Panache:{
-        "name": "PANACHE",
-        "venue": "Central Lawn, MNIT Jaipur",
-        "day": 2,
-        "rulebook": "Will be shared later",
-        "description": "PANACHE 2025 is a fashion show organized by Malaviya National Institute of Technology, aimed at redefining style, expressiveness, charisma, and glamour. It brings an opportunity to showcase the best trends, both past and present, with creativity, panache, and confidence.",
-        "maxTeamSize": 15,
-        "eventPath": "panache",
-        "imgUrl": "src/Assets/panache.jpg",
-        "club": "none",
-        "type": "flagship"
+    panache:{
+        name: "PANACHE",
+        venue: "Central Lawn, MNIT Jaipur",
+        day: 2,
+        rulebook: "Will be shared later",
+        description: "PANACHE 2025 is a fashion show organized by Malaviya National Institute of Technology, aimed at redefining style, expressiveness, charisma, and glamour. It brings an opportunity to showcase the best trends, both past and present, with creativity, panache, and confidence.",
+        maxTeamSize: 15,
+        eventPath: "panache",
+        imgUrl: "src/Assets/panache.jpg",
+        club: "none",
+        type: "flagship"
     },
-    Tamasha:{
-        "name": "TAMASHA",
-        "venue": "SAC Lawn, MNIT Jaipur",
-        "day": 2,
-        "rulebook": "Will be shared later",
-        "description": "TAMASHA is a celebration of the raw, unfiltered power of storytelling and social commentary through Nukkad Natak. It blends laughter with social commentary, reminding you that sometimes the real show happens off the main stage.",
-        "maxTeamSize": 15,
-        "eventPath": "tamasha",
-        "imgUrl": "src/Assets/tamasha.webp",
-        "club": "none",
-        "type": "flagship"
+    tamasha:{
+        name: "TAMASHA",
+        venue: "SAC Lawn, MNIT Jaipur",
+        day: 2,
+        rulebook: "Will be shared later",
+        description: "TAMASHA is a celebration of the raw, unfiltered power of storytelling and social commentary through Nukkad Natak. It blends laughter with social commentary, reminding you that sometimes the real show happens off the main stage.",
+        maxTeamSize: 15,
+        eventPath: "tamasha",
+        imgUrl: "src/Assets/tamasha.webp",
+        club: "none",
+        type: "flagship"
     },
-    BattleOfBands:{
-        "name": "BATTLE OF BANDS",
-        "venue": "VLTC Back Parking, MNIT Jaipur",
-        "day": 3,
-        "rulebook": "Will be shared later",
-        "description": "BATTLE OF BANDS is where amplified passion meets fierce competition. Watch local bands compete for the coveted title of 'Best Band on Campus,' showcasing their original compositions and captivating the audience with their musical mastery.",
-        "maxTeamSize": 10,
-        "eventPath": "battle-of-bands",
-        "imgUrl": "src/Assets/battle_of_bands.jpg",
-        "club": "none",
-        "type": "flagship"
+    battleofbands:{
+        name: "BATTLE OF BANDS",
+        venue: "VLTC Back Parking, MNIT Jaipur",
+        day: 3,
+        rulebook: "Will be shared later",
+        description: "BATTLE OF BANDS is where amplified passion meets fierce competition. Watch local bands compete for the coveted title of 'Best Band on Campus,' showcasing their original compositions and captivating the audience with their musical mastery.",
+        maxTeamSize: 10,
+        eventPath: "battleofbands",
+        imgUrl: "src/Assets/battle_of_bands.jpg",
+        club: "none",
+        type: "flagship"
     },
-    RambaSamba:{
-        "name": "RAMBA SAMBA",
-        "venue": "Central Lawn & OAT, MNIT Jaipur",
-        "day": 3,
-        "rulebook": "Will be shared later",
-        "description": "RAMBA SAMBA 2025 offers a captivating journey through diverse dance styles, blending traditional and contemporary movements. Teams will showcase their creativity through breathtaking choreographies, electrifying energy, and heart-throbbing performances, celebrating the magic of collective movement.",
-        "maxTeamSize": 15,
-        "eventPath": "ramba-samba",
-        "imgUrl": "src/Assets/ramba_samba.jpg",
-        "club": "none",
-        "type": "flagship"
+    rambasamba:{
+        name: "RAMBA SAMBA",
+        venue: "Central Lawn & OAT, MNIT Jaipur",
+        day: 3,
+        rulebook: "Will be shared later",
+        description: "RAMBA SAMBA 2025 offers a captivating journey through diverse dance styles, blending traditional and contemporary movements. Teams will showcase their creativity through breathtaking choreographies, electrifying energy, and heart-throbbing performances, celebrating the magic of collective movement.",
+        maxTeamSize: 15,
+        eventPath: "rambasamba",
+        imgUrl: "src/Assets/ramba_samba.jpg",
+        club: "none",
+        type: "flagship"
     }
     
     
