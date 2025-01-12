ca355e95eeea3e5c383521fac9eead21984e70f9
Sachin-og <2022uec1128@mnit.ac.in>
2025-01-12 12:39:29 +0530

    main34

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Allow access from other devices on the network
-    port: 3000, // You can change the port if you like
+    port: 3000, // Change the port if needed
+    open: true, // Automatically open the app in the browser
+    historyApiFallback: true, // Redirect all requests to index.html
+  },
+  build: {
+    rollupOptions: {
+      input: './index.html', // Ensure the main entry point is index.html
+    },
   },
 })
