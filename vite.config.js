837e8426d4f3226df1ec8156f5bf2771a6131c9d
Sachin-og <2022uec1128@mnit.ac.in>
2025-01-12 12:47:35 +0530

    main

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,11 +8,10 @@ export default defineConfig({
     host: '0.0.0.0', // Allow access from other devices on the network
     port: 3000, // Change the port if needed
     open: true, // Automatically open the app in the browser
-    historyApiFallback: true, // Redirect all requests to index.html
   },
   build: {
     rollupOptions: {
       input: './index.html', // Ensure the main entry point is index.html
     },
   },
-})
+});
