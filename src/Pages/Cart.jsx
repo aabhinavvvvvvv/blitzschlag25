0a32c35415d669e157ebf5a0355b6e6932cb96e6
AnantHansras <ananthansras.16@gmail.com>
2025-01-17 20:54:12 +0530

    Responsivness done

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,27 +1,58 @@
-import React,{useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Trash2, MinusIcon, PlusIcon } from 'lucide-react';
 import { removeFromCart, updateCartItemQuantity } from '../slices/cartSlice'; // Import your cartSlice actions
 import bg from '../Assets/passbg.webp';
 import { FaArrowLeft } from "react-icons/fa";
-
+import { FaArrowRight } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../../firebase';
+import { toast } from 'react-toastify';
 function Cart() {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const cartItems = useSelector(state => state.cart.cartItems); // Access cart items from Redux store
-  const [totalCartAmount,settotalCartAmount] = useState(cartItems.reduce((sum, item) => sum + item.totalAmount, 0));
+  const [totalCartAmount, setTotalCartAmount] = useState(0);
+
+  // Recalculate totalCartAmount whenever cartItems change
+  useEffect(() => {
+    const total = cartItems.reduce((sum, item) => sum + item.totalAmount * item.quantity, 0);
+    setTotalCartAmount(total);
+  }, [cartItems]);
+
   const deleteItem = (item) => {
+    toast.success("Removed from cart");
     dispatch(removeFromCart(item)); // Dispatch action to remove item from cart
-    settotalCartAmount(cartItems.reduce((sum, item) => sum + item.totalAmount, 0))
   };
 
   const updateQuantity = (item, newQuantity) => {
     if (newQuantity > 0) {
       dispatch(updateCartItemQuantity({ item, newQuantity })); // Dispatch action to update item quantity
     }
-    settotalCartAmount(cartItems.reduce((sum, item) => sum + item.totalAmount, 0))
   };
 
+  // *****************************************//
+  const [uid, setUid] = useState(null);
+  useEffect(() => {
+    const user = auth.currentUser;
+    if (user) {
+      setUid(user.uid); // Set UID when the user is logged in
+    } else {
+      toast.error("User not logged in.");
+    }
+  }, []);
 
+  const handleOnclick = (e) => {
+    e.preventDefault();
+    navigate('/pay', {
+      state: {
+        passDetails: cartItems,
+        amount: totalCartAmount,
+        userId: uid
+      },
+    });
+  }
+  // *****************************************//
 
   return (
     <div className="min-h-screen w-full bg-transparent relative overflow-y-auto font-sans" style={{ fontFamily: 'cursive' }}>
@@ -29,59 +60,70 @@ function Cart() {
         className="fixed inset-0 bg-cover bg-center"
         style={{ backgroundImage: `url(${bg})` }}
       ></div>
-      <div className="bg-black bg-opacity-30 relative h-screen">
+      <div className="bg-black bg-opacity-30 relative min-h-screen">
         <div className="max-w-3xl mx-auto px-4 py-12">
           <h1 className="text-5xl font-bold mt-8 mb-8 text-center text-gray-200 tracking-wider" style={{ fontFamily: "'Metal Mania', cursive" }}>
             Your Cart
           </h1>
-          <div className="space-y-6">
-            {cartItems.map((item, index) => (
-              <div
-                key={index}
-                className="relative flex flex-col sm:flex-row items-center p-6 rounded-xl border border-gray-200 backdrop-filter backdrop-blur-lg shadow-xl transition-all duration-300"
-              >
-                <div className="flex-grow mb-4 sm:mb-0">
-                  <h2 className="text-2xl font-semibold text-white mb-2">{item.passName}</h2>
-                  {item.day && <p className="text-gray-300">{item.day}</p>}
-                  {item.flagshipEvent && <p className="text-gray-300">{item.flagshipEvent}</p>}
-                  <p className="text-gray-300 mt-2">Quantity: {item.quantity}</p>
-                  <p className="text-xl font-bold text-indigo-400 mt-2">Total: ₹{item.totalAmount}</p>
+          {
+            cartItems.length === 0 ? (
+              <div className='flex justify-center my-40'>
+                <p className='text-5xl font-bold text-white'>Your cart is empty</p>
+              </div>
+            ) : (
+              <div>
+                <div className="space-y-6">
+                  {cartItems.map((item, index) => (
+                    <div
+                      key={index}
+                      className="relative flex flex-col sm:flex-row items-center p-3 md:p-6 rounded-xl border border-gray-200 backdrop-filter backdrop-blur-lg shadow-xl transition-all duration-300"
+                    >
+                      <div className="flex-grow mb-4 sm:mb-0">
+                        <h2 className="text-2xl font-semibold text-white mb-2">{item.passName}</h2>
+                        {item.day && <p className="text-gray-300">{item.day}</p>}
+                        {item.flagshipEvent && <p className="text-gray-300">{item.flagshipEvent}</p>}
+                        <p className="text-gray-300 mt-2">Quantity: {item.quantity}</p>
+                        <p className="text-xl font-bold text-indigo-400 mt-2">Total: ₹{item.totalAmount * item.quantity}</p>
+                      </div>
+                      <div className="flex items-center space-x-4">
+                        <div className="flex items-center space-x-2">
+                          <button
+                            onClick={() => updateQuantity(item, item.quantity - 1)}
+                            className="bg-indigo-100 text-indigo-600 hover:bg-indigo-200 p-2 rounded-full"
+                          >
+                            <MinusIcon className="h-4 w-4" />
+                          </button>
+                          <span className="text-white font-semibold">{item.quantity}</span>
+                          <button
+                            onClick={() => updateQuantity(item, item.quantity + 1)}
+                            className="bg-indigo-100 text-indigo-600 hover:bg-indigo-200 p-2 rounded-full"
+                          >
+                            <PlusIcon className="h-4 w-4" />
+                          </button>
+                        </div>
+                        <button
+                          onClick={() => deleteItem(item)}
+                          className="bg-red-500 hover:bg-red-600 text-white p-2 rounded-full"
+                        >
+                          <Trash2 className="h-4 w-4" />
+                        </button>
+                      </div>
+                    </div>
+                  ))}
                 </div>
-                <div className="flex items-center space-x-4">
-                  <div className="flex items-center space-x-2">
-                    <button
-                      onClick={() => updateQuantity(item, item.quantity - 1)}
-                      className="bg-indigo-100 text-indigo-600 hover:bg-indigo-200 p-2 rounded-full"
-                    >
-                      <MinusIcon className="h-4 w-4" />
-                    </button>
-                    <span className="text-white font-semibold">{item.quantity}</span>
-                    <button
-                      onClick={() => updateQuantity(item, item.quantity + 1)}
-                      className="bg-indigo-100 text-indigo-600 hover:bg-indigo-200 p-2 rounded-full"
-                    >
-                      <PlusIcon className="h-4 w-4" />
-                    </button>
-                  </div>
-                  <button
-                    onClick={() => deleteItem(item)}
-                    className="bg-red-500 hover:bg-red-600 text-white p-2 rounded-full"
-                  >
-                    <Trash2 className="h-4 w-4" />
-                  </button>
+                <div className="mt-12  flex justify-center items-center">
+                  <p className="text-3xl md:text-5xl font-bold text-white">Cart Total: ₹{totalCartAmount}</p>
                 </div>
               </div>
-            ))}
-          </div>
-          <div className="mt-12 text-right flex justify-center item-center">
-            <p className="text-5xl font-bold text-white">Cart Total: ₹{totalCartAmount}</p>
-          </div>
-          <div className="mt-8 flex justify-between">
-            <button className="px-6 py-3 flex flex-row items-center rounded-lg font-semibold text-white bg-indigo-600 hover:bg-indigo-500">
-            <FaArrowLeft className='mr-2'/><p>Back to passes</p>
+            )
+          }
+
+          <div className="mt-8 flex flex-col sm:flex-row justify-between items-center space-y-4 sm:space-y-0">
+            <button onClick={() => { navigate('/pass'); }} className="p-3 md:px-6 md:py-3 flex flex-row items-center rounded-lg font-semibold text-white bg-indigo-600 hover:bg-indigo-500">
+              <FaArrowLeft className='mr-2' /><p>Back to passes</p>
             </button>
-            <button className="px-6 py-3 rounded-lg font-semibold text-white bg-indigo-600 hover:bg-indigo-500">
-              Proceed to Buy
+            <button onClick={handleOnclick} className="p-3 md:px-6 md:py-3 rounded-lg flex flex-row items-center font-semibold text-white bg-indigo-600 hover:bg-indigo-500">
+              Proceed to Buy<FaArrowRight className='ml-2'/>
             </button>
           </div>
         </div>
@@ -91,5 +133,7 @@ function Cart() {
 }
 
 export default Cart;
+
+
 
 
--- src/Pages/Pass.jsx
+++ src/Pages/Pass.jsx
@@ -4,6 +4,113 @@ import bg from '../Assets/passbg.webp'
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { addToCart } from '../slices/cartSlice';
+import styled from 'styled-components';
+import { toast } from 'react-toastify';
+const Button = () => {
+  const navigate = useNavigate();
+  return (
+    <StyledWrapper>
+      <button onClick={() => navigate('/cart')} className="button">
+        <div className="bgContainer">
+          <span>Cart </span>
+          <span>Cart </span>
+        </div>
+        <div className="arrowContainer">
+          <svg width={25} height={25} viewBox="0 0 45 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M43.7678 20.7678C44.7441 19.7915 44.7441 18.2085 43.7678 17.2322L27.8579 1.32233C26.8816 0.34602 25.2986 0.34602 24.3223 1.32233C23.346 2.29864 23.346 3.88155 24.3223 4.85786L38.4645 19L24.3223 33.1421C23.346 34.1184 23.346 35.7014 24.3223 36.6777C25.2986 37.654 26.8816 37.654 27.8579 36.6777L43.7678 20.7678ZM0 21.5L42 21.5V16.5L0 16.5L0 21.5Z" fill="black" />
+          </svg>
+        </div>
+      </button>
+    </StyledWrapper>
+  );
+}
+
+const StyledWrapper = styled.div`
+  button {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 1em 0em 1em 1em;
+    background-color: pink;
+    cursor: pointer;
+    box-shadow: 4px 6px 0px black;
+    border: 4px solid;
+    border-radius: 15px;
+    position: relative;
+    overflow: hidden;
+    z-index: 100;
+    transition: box-shadow 250ms, transform 250ms, filter 50ms;
+  }
+  button:hover {
+    transform: translate(2px, 2px);
+    box-shadow: 2px 3px 0px black;
+  }
+  button:active {
+    filter: saturate(0.75);
+  }
+  button::after {
+    content: "";
+    position: absolute;
+    inset: 0;
+    background-color: yellow;
+    z-index: -1;
+    transform: translateX(-100%);
+    transition: transform 250ms;
+  }
+  button:hover::after {
+    transform: translateX(0);
+  }
+  .bgContainer {
+    position: relative;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    overflow: hidden;
+    max-width: 33%; /* adjust this if the button text is not proper */
+    font-size: 2em;
+    font-weight: 600;
+  }
+  .bgContainer span {
+    position: relative;
+    transform: translateX(-100%);
+    transition: all 250ms;
+  }
+  .button:hover .bgContainer > span {
+    transform: translateX(0);
+  }
+  .arrowContainer {
+    padding: 1em;
+    margin-inline-end: 1em;
+    border: 4px solid;
+    border-radius: 50%;
+    background-color: yellow;
+    position: relative;
+    overflow: hidden;
+    transition: transform 250ms, background-color 250ms;
+    z-index: 100;
+  }
+  .arrowContainer::after {
+    content: "";
+    position: absolute;
+    inset: 0;
+    border-radius: inherit;
+    background-color: pink;
+    transform: translateX(-100%);
+    z-index: -1;
+    transition: transform 250ms ease-in-out;
+  }
+  button:hover .arrowContainer::after {
+    transform: translateX(0);
+  }
+  button:hover .arrowContainer {
+    transform: translateX(5px);
+  }
+  button:active .arrowContainer {
+    transform: translateX(8px);
+  }
+  .arrowContainer svg {
+    vertical-align: middle;
+  }`;
 
 const passes = [
   {
@@ -127,15 +234,16 @@ function PassCard({ pass }) {
       const details = {
         passName: pass.name,
         quantity,
-        totalAmount: pass.price * quantity,
+        totalAmount: pass.price,
         ...(pass.daySelection && { day: selectedDay }),
         ...(pass.flagshipSelection && { flagshipEvent: selectedFlagship }),
       };
       
       // Dispatch the action to add to cart
       dispatch(addToCart(details));
+      toast.success("Succesfully added to cart");
     } else {
-      alert("Quantity must be greater than 0");
+      toast.error("Quantity must be greater than 0");
     }
   };
   return (
@@ -243,16 +351,17 @@ export default function Pass() {
               Cultural Fest Passes
             </h3>
           </div>
-          <button
-            onClick={() => navigate('/cart')}
-            className="px-4 py-2 mb-8 rounded-lg font-semibold text-white bg-green-600 hover:bg-green-500 active:bg-green-700"
-          >
-            Go to Cart
-          </button>
+          
           <div className="mt-16 grid gap-6 sm:grid-cols-2 lg:grid-cols-3 justify-center items-stretch">
             {passes.map((pass, idx) => (
               <PassCard key={idx} pass={pass} />
             ))}
+            <p></p>
+            <div className='flex justify-center items-center  md:-ml-96'>
+            <Button >Go to Cart</Button>
+            </div>
+
+            
           </div>
         </div>
       </section>
