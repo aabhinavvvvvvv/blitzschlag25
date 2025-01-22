249f6bfabdf101f476d3df5d11a34644870f4a56
Kavyansh-Bagdi <kavyanshbagdi224@gmail.com>
2025-01-22 19:26:52 +0530

    nav bar edited

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -93,7 +93,7 @@ const [isOpen, setIsOpen] = useState(false);
   };
 
   return (
-    <div className="absolute w-full z-50 bg-transparent py-3">
+    <div className="absolute w-full z-50 bg-transparent">
       {
           isOpen && <QRmodal  toggleModal={toggleModal} />
       }
@@ -105,19 +105,19 @@ const [isOpen, setIsOpen] = useState(false);
             <img
               src={logo}
               alt="Blitz Logo"
-              className="h-20 p-1 ml-3 absolute top-0"
+              className="h-20 p-1 ml-3 absolute top-1"
             />
           </Link>
         </div>
 
         {/* Main Navigation (Visible in PC) */}
         <div
-          className="hidden relative p-2 -top-3 font-normal text-2xl bg-[#C4C4C430] rounded-b-xl  text-[#D3D3D3] lg:flex justify-between items-center gap-x-7"
+          className="hidden relative font-normal p-1 top-3 left-12 text-2xl bg-[#C4C4C430] rounded-xl  text-[#D3D3D3] lg:flex justify-between items-center gap-x-7"
           style={{ fontFamily: "'Jaro', sans-serif" }}
         >
           <Link
             to="/sponsor"
-            className="px-4 py-2 hover:text-white hover:font-bold transition-all duration-200"
+            className=" px-4 py-2 hover:text-white hover:font-bold transition-all duration-200"
           >
             Sponsor
           </Link>
